27788cd8ee0344a11e4a76ac9d4ca5423dfd41d7
TheAihopGG <theaihopgg@gmail.com>
2025-04-12 23:05:18 +0700

    feat: annotations

--- scenes/characters/character/character.gd
+++ scenes/characters/character/character.gd
@@ -7,7 +7,7 @@ class_name Character
 @export var movement_is_enabled: bool = true
 @export var attack_is_enabled: bool = true
 
-@onready var animation_player = get_node("%AnimationPlayer")
-@onready var animated_sprite_2d = get_node("%AnimatedSprite2D")
+@onready var animation_player: AnimationPlayer = get_node("%AnimationPlayer")
+@onready var animated_sprite_2d: AnimatedSprite2D = get_node("%AnimatedSprite2D")
 
 var move_direction: Vector2 = default_move_direction
