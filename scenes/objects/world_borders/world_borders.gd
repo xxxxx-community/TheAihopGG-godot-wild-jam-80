5ce52923be8b7a8f2f74c8b540299ff391527902
TheAihopGG <theaihopgg@gmail.com>
2025-04-12 22:08:42 +0700

    fix: x_distance and y_distance sign

--- scenes/objects/world_borders/world_borders.gd
+++ scenes/objects/world_borders/world_borders.gd
@@ -7,9 +7,9 @@ class_name WorldBorders
 @onready var left_border: CollisionShape2D = get_node("BorderLeft")
 
 func _set_borders():
-	var x_distance: float = GlobalVars.world.world_size.x / 2
-	var y_distance: float = GlobalVars.world.world_size.y / 2
-	top_border.shape.distance = -y_distance
-	bottom_border.shape.distance = -y_distance
-	right_border.shape.distance = -x_distance
-	left_border.shape.distance = -x_distance
+	var x_distance: float = -(GlobalVars.world.world_size.x / 2)
+	var y_distance: float = -(GlobalVars.world.world_size.y / 2)
+	top_border.shape.distance = y_distance
+	bottom_border.shape.distance = y_distance
+	right_border.shape.distance = x_distance
+	left_border.shape.distance = x_distance
