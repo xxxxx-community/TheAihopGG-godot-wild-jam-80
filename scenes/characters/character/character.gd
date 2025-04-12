ea0213eeb1262b347674a6e79b2bffd47be77645
TheAihopGG <theaihopgg@gmail.com>
2025-04-12 22:36:50 +0700

    feat: character base

--- scenes/characters/character/character.gd
+++ scenes/characters/character/character.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+class_name Character
+
+@export var character_name: String
+@export var speed: float
+@export var default_move_direction: Vector2 = Vector2.RIGHT
+@export var movement_is_enabled: bool = true
+@export var attack_is_enabled: bool = true
+
+@onready var animation_player = get_node("%AnimationPlayer")
+@onready var animated_sprite_2d = get_node("%AnimatedSprite2D")
+
+var move_direction: Vector2 = default_move_direction
