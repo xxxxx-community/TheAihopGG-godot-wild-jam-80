6ac94a18daf946fef7133b37fc969ef040dfa82d
TheAihopGG <theaihopgg@gmail.com>
2025-04-13 12:18:08 +0700

    fix: labels

--- scenes/characters/character/character.gd
+++ scenes/characters/character/character.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 class_name Character
 
+@onready var health_component: HealthComponent = get_node("%HealthComponent")
+@onready var animation_player: AnimationPlayer = get_node("%AnimationPlayer")
+@onready var animated_sprite_2d: AnimatedSprite2D = get_node("%AnimatedSprite2D")
+@onready var health_label: Label = get_node("%HealthLabel")
+@onready var speed_label: Label = get_node("%SpeedLabel")
+@onready var name_label: Label = get_node("%NameLabel")
+
 @export var character_name: String
 @export_category("Movement")
 @export var speed: float
@@ -14,16 +21,24 @@ class_name Character
 @export var health_replenish_is_enabled: bool = true
 @export var invulnerability_is_enabled: bool = false # note: don t use invulnerability_is_enabled, use health_component.invulnerability_is_enabled
 @export_category("Labels")
-@export var show_health_label: bool = false
-@export var show_name_label: bool = true
-@export var show_speed_label: bool = true
-
-@onready var health_component: HealthComponent = get_node("%HealthComponent")
-@onready var animation_player: AnimationPlayer = get_node("%AnimationPlayer")
-@onready var animated_sprite_2d: AnimatedSprite2D = get_node("%AnimatedSprite2D")
-@onready var health_label: Label = get_node("%HealthLabel")
-@onready var speed_label: Label = get_node("%SpeedLabel")
-@onready var name_label: Label = get_node("%NameLabel")
+@export var show_health_label: bool = false:
+	set(value):
+		print(value)
+		show_health_label = value
+		if health_label:
+			health_label.visible = value
+@export var show_name_label: bool = false:
+	set(value):
+		print(value)
+		show_name_label = value
+		if name_label:
+			name_label.visible = value
+@export var show_speed_label: bool = false:
+	set(value):
+		print(value)
+		show_speed_label = value
+		if speed_label:
+			speed_label.visible = value
 
 var move_direction: Vector2 = default_move_direction
 
@@ -33,6 +48,9 @@ func _ready():
 	health_component.invulnerability_time = invulnerability_time
 	health_component.health_replenish_is_enabled = health_replenish_is_enabled
 	health_component.invulnerability_is_enabled = invulnerability_is_enabled
+	health_label.visible = show_health_label
+	name_label.visible = show_name_label
+	speed_label.visible = show_speed_label
 
 func _process(_delta):
 	if movement_is_enabled:
@@ -42,8 +60,5 @@ func _process(_delta):
 
 func show_labels():
 	health_label.text = "health: " + str(health_component.health)
-	health_label.visible = show_health_label
 	speed_label.text = "speed: " + str(speed)
-	speed_label.visible = show_speed_label
 	name_label.text = "name: " + str(character_name)
-	name_label.visible = show_name_label
--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -3,13 +3,20 @@ class_name Player
 
 func _ready():
 	GlobalVars.player = self
-	# TODO: fix
 	health_component.health = health
 	health_component.max_health = max_health
 	health_component.invulnerability_time = invulnerability_time
 	health_component.health_replenish_is_enabled = health_replenish_is_enabled
 	health_component.invulnerability_is_enabled = invulnerability_is_enabled
+	health_label.visible = show_health_label
+	name_label.visible = show_name_label
+	speed_label.visible = show_speed_label
 
 func _process(delta: float) -> void:
 	move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if Input.is_action_just_pressed("ui_home"):
+		if show_health_label:
+			show_health_label = false
+		else:
+			show_health_label = true
 	super._process(delta)
