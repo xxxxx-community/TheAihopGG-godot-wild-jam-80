63c5077c2aee0bfa6564e837ada155fa1cf08b40
TheAihopGG <theaihopgg@gmail.com>
2025-04-13 11:38:36 +0700

    feat: labels

--- scenes/characters/character/character.gd
+++ scenes/characters/character/character.gd
@@ -8,17 +8,22 @@ class_name Character
 @export var movement_is_enabled: bool = true
 @export var attack_is_enabled: bool = true
 @export_category("Health")
-@export var show_health: bool = false
 @export var health: int
 @export var max_health: int
 @export var invulnerability_time: float
 @export var health_replenish_is_enabled: bool = true
 @export var invulnerability_is_enabled: bool = false # note: don t use invulnerability_is_enabled, use health_component.invulnerability_is_enabled
+@export_category("Labels")
+@export var show_health_label: bool = false
+@export var show_name_label: bool = true
+@export var show_speed_label: bool = true
 
 @onready var health_component: HealthComponent = get_node("%HealthComponent")
 @onready var animation_player: AnimationPlayer = get_node("%AnimationPlayer")
 @onready var animated_sprite_2d: AnimatedSprite2D = get_node("%AnimatedSprite2D")
 @onready var health_label: Label = get_node("%HealthLabel")
+@onready var speed_label: Label = get_node("%SpeedLabel")
+@onready var name_label: Label = get_node("%NameLabel")
 
 var move_direction: Vector2 = default_move_direction
 
@@ -36,5 +41,9 @@ func _process(_delta):
 	move_and_slide()
 
 func show_labels():
-	health_label.text = str(health_component.health)
-	health_label.visible = show_health
+	health_label.text = "health: " + str(health_component.health)
+	health_label.visible = show_health_label
+	speed_label.text = "speed: " + str(speed)
+	speed_label.visible = show_speed_label
+	name_label.text = "name: " + str(character_name)
+	name_label.visible = show_name_label
